d49a012bf4cbc52da1e6e13672c9931c10050ff8
Marquis Ogre <marquisogre@gmail.com>
2022-02-28 03:25:45 +0530

    Increase footer logo size

--- packages/digiswap-uikit/src/components/Footer/Footer.tsx
+++ packages/digiswap-uikit/src/components/Footer/Footer.tsx
@@ -61,7 +61,7 @@ const MenuItem: React.FC<FooterProps> = ({
             </StyledList>
           ))}
           <Box display={["none", null, "block"]}>
-            <LogoWithTextIcon isDark width="160px" />
+            <LogoWithTextIcon isDark width="260px" />
           </Box>
         </Flex>
         <StyledSocialLinks order={[2]} pb={["42px", null, "32px"]} mb={["0", null, "32px"]} />
